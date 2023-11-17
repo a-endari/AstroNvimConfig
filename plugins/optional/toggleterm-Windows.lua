1e4ec50b256f9aeeac7a9794e73753fd460e06a8
Abbas Endari <a.endari@gmail.com>
2023-11-17 14:13:57 +0330

    windows settings update

--- plugins/optional/toggleterm-Windows.lua
+++ plugins/optional/toggleterm-Windows.lua
@@ -1,17 +1,17 @@
 return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-        size = 10,
-        open_mapping = [[<C-\>]],
-        start_in_insert = true,
-        direction = "float",
-        -- settings to make toggle term open powershell in windows!
-        shell = "pwsh.exe",
-        float_opts = {
-            border = "curved",
-            width = math.ceil(vim.o.columns * 0.8),
-            height = math.ceil(vim.o.columns * 0.2)
-        }
-    }
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = 10,
+    open_mapping = [[<C-\>]],
+    start_in_insert = true,
+    direction = "float",
+    -- settings to make toggle term open powershell in windows!
+    shell = "pwsh.exe -nologo",
+    float_opts = {
+      border = "curved",
+      width = math.ceil(vim.o.columns * 0.8),
+      height = math.ceil(vim.o.columns * 0.2),
+    },
+  },
 }
